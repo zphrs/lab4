ac9e908e9fa6b7ee0e6f224107e47d3e794882da
zphyrj <46908127+zphyrj@users.noreply.github.com>
2020-07-20 06:21:31 -0700

    fixed border

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -10,7 +10,7 @@ class Scatter {
     // Configs
     svgW = 360;
     svgH = 360;
-    gMargin = {top: 50, right: 50, bottom: 50, left: 50};
+    gMargin = {top: 20, right: 0, bottom: 50, left: 50};
     gW = this.svgW - (this.gMargin.right + this.gMargin.left);
     gH = this.svgH - (this.gMargin.top + this.gMargin.bottom);
     domain = null;
